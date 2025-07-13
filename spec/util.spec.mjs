9847226d8bbb175db30a8e1251967733ef6eac71
sri2456 <ksri2456@gmail.com>
2025-07-13 12:35:59 -0400

    Update util.spec.mjs

--- spec/util.spec.mjs
+++ spec/util.spec.mjs
@@ -1,1 +1,13 @@
+import { displayTodayDate } from "../src/util.js";
+
+describe('first sample tests', () => {
+    it('should display today date', () => {
+        const element = document.createElement('div');
+        element.setAttribute('id', 'date');
+        document.body.appendChild(element);
+        displayTodayDate();
+        let date = new Date().toDateString();
+        expect(element.innerText).toEqual(date);
+    });
+});
 
